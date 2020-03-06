9a4d1f467d9a68e5d7fe38ff411d159d94ac5b66
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-03-06 09:37:27 +0400

    Initial commit

---
+++ src/main/java/com/company/sample/screen/main/SampleMainScreen.java
@@ -0,0 +1,12 @@
+package com.company.sample.screen.main;
+
+import io.jmix.ui.screen.LoadDataBeforeShow;
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("sample_MainScreen")
+@UiDescriptor("sample-main-screen.xml")
+@LoadDataBeforeShow
+public class SampleMainScreen extends Screen {
+}
