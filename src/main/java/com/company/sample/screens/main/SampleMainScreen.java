9b52b6fbf8e6e9c0c7ba206ca27192d3b28ccea9
Gleb Gorelov <gorelov@haulmont.com>
2020-04-29 13:26:47 +0400

    Add custom component

--- src/main/java/com/company/sample/screens/main/SampleMainScreen.java
+++ src/main/java/com/company/sample/screens/main/SampleMainScreen.java
@@ -1,11 +1,11 @@
 package com.company.sample.screens.main;
 
+import com.company.sample.widgets.CustomButton;
+import com.vaadin.ui.Layout;
 import io.jmix.ui.components.AppWorkArea;
+import io.jmix.ui.components.VBoxLayout;
 import io.jmix.ui.components.Window;
-import io.jmix.ui.screen.LoadDataBeforeShow;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -24,4 +24,14 @@ public class SampleMainScreen extends Screen implements Window.HasWorkArea {
         return workArea;
     }
 
+    @Inject
+    private VBoxLayout customBtnBox;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        CustomButton button = new CustomButton();
+        button.setColor("#ff00ff");
+        button.setCaption("Custom Button");
+        customBtnBox.unwrap(Layout.class).addComponent(button);
+    }
 }
