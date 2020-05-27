ceccfbf6f4346d402b82c5bb155bf83140047fac
Roman Pinyazhin <pinyazhin@haulmont.com>
2020-05-27 15:45:32 +0400

    Init menu logo image

--- src/main/java/com/company/sample/screen/main/SampleMainScreen.java
+++ src/main/java/com/company/sample/screen/main/SampleMainScreen.java
@@ -1,11 +1,12 @@
 package com.company.sample.screen.main;
 
+import io.jmix.core.Messages;
 import io.jmix.ui.component.AppWorkArea;
+import io.jmix.ui.component.Image;
+import io.jmix.ui.component.ThemeResource;
 import io.jmix.ui.component.Window;
-import io.jmix.ui.screen.LoadDataBeforeShow;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -15,8 +16,15 @@ import javax.inject.Inject;
 @LoadDataBeforeShow
 public class SampleMainScreen extends Screen implements Window.HasWorkArea {
 
+    private static final String APP_LOGO_IMAGE = "application.logoImage";
+    
     @Inject
     private AppWorkArea workArea;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        initLogoImage();
+    }
 
     @Nullable
     @Override
@@ -24,4 +32,15 @@ public class SampleMainScreen extends Screen implements Window.HasWorkArea {
         return workArea;
     }
 
+    private void initLogoImage() {
+        Image logoImage = (Image) getWindow().getComponent("logoImage");
+        String logoImagePath = getBeanLocator().get(Messages.class)
+                .getMessage(APP_LOGO_IMAGE);
+
+        if (logoImage != null
+                && StringUtils.isNotBlank(logoImagePath)
+                && !APP_LOGO_IMAGE.equals(logoImagePath)) {
+            logoImage.setSource(ThemeResource.class).setPath(logoImagePath);
+        }
+    }
 }
